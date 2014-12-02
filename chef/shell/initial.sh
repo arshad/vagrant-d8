a3fe4e9917ce804764f187b19eab524d52ec4efd
Arshad Chummun <arshad@chummun.com>
2014-12-02 23:08:13 +0400

    Reverted back /dev/null

--- chef/shell/initial.sh
+++ chef/shell/initial.sh
@@ -9,5 +9,5 @@ cat "$VAGRANT_DIR/chef/shell/vdd.txt"
 echo "Updating Chef to 11.12.4 version. This may take a few minutes..."
 apt-get update &> /dev/null
 apt-get install build-essential ruby1.9.1-dev --no-upgrade --yes &> /dev/null
-#gem install chef --version 11.12.4 --no-rdoc --no-ri --conservative &> /dev/null
-gem install chef --version 11.12.4 --no-rdoc --no-ri --conservative
+gem install chef --version 11.12.4 --no-rdoc --no-ri --conservative &> /dev/null
+#gem install chef --version 11.12.4 --no-rdoc --no-ri --conservative
